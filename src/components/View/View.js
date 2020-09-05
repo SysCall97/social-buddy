7d9ea8fed85484956660a85fd1852982d54798a8
Kazi Mashry <kazimashry@gmail.com>
2020-09-05 09:57:57 +0600

    router set

--- src/components/View/View.js
+++ src/components/View/View.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import Header from '../Header/Header';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+import Home from '../Home/Home';
+import PageNotFound from '../PageNotFound/PageNotFound';
+import { Container } from '@material-ui/core';
 
 const View = () => {
     return (
         <>
             <Header />
+            <Container>
+                <Router>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/home">
+                            <Home />
+                        </Route>
+
+                        <Route path="*">
+                            <PageNotFound />
+                        </Route>
+
+                    </Switch>
+                </Router>
+            </Container>
         </>
     );
 };
